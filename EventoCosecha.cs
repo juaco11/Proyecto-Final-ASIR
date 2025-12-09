b14a3d9602e656d7f471e668de58c61406524831
juaco11 <joaquingarciacarbonell.11.11@gmail.com>
2025-12-09 13:18:51 +0100

    Add files via upload

---
+++ EventoCosecha.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// En EventoCosecha.cs
+namespace Fruteria.Modelos
+{
+    public class EventoCosecha
+    {
+        // Datos del formulario del agricultor
+        public string? AgricultorID { get; set; } 
+        public string? Variedad { get; set; }
+        public double Kilos { get; set; }
+        public string? Origen { get; set; } // Ej: "Finca El Sol, Alicante"
+
+        // Datos que añadiremos nosotros
+        public string? LoteID { get; set; } // Un ID único (Ej: "MANZ-001")
+        public string? ImagenUrl { get; set; } // La URL de la imagen
+        public DateTime FechaCosecha { get; set; }
+    }
+}
